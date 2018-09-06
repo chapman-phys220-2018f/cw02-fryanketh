a01831d4079b374cc9f804e6b6b75dda5e2cc7e6
Frank <entriken@chapman.edu>
2018-09-06 17:43:50 +0000

    Changed line 3 and 5

--- fixed_script.sh
+++ fixed_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "This script has typos. Please fix them."
-# You will also change this line later
-
+# This is a different conflicting comment.
+# This is an unrelated comment
 echo "Positive integers from 005 to 100:"
 for i in $(seq -w ${5-100} ); do echo -n "$i "; done; echo
 
